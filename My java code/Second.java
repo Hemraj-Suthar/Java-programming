00b1167df7e1b996c4aae9e4a1b25a334dcf4cf4
Hemraj Suthar <hemrajct@gmail.com>
2024-02-12 16:41:58 +0530

    commit

---
+++ My java code/Second.java
@@ -0,0 +1,10 @@
+public class Second {
+
+    static void myFun(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public static void main(String[] args) {
+        myFun(5, 10);
+    }
+}
